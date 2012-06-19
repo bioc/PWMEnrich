4bccb91dcd65b1e94a87a3673b6606595c1395c9
Robert Stojnic <robert.stojnic@gmail.com>
2012-06-19 11:01:33 +0000

    Forgot to change the package name in test cases, move doMC to Suggest to prevent the build in windows from failing
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/PWMEnrich@66821 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/tests/test_similarity.R
+++ inst/tests/test_similarity.R
@@ -2,7 +2,7 @@ library(PWMEnrich)
 library(testthat)
 
 # test function for motif similarity
-motifs = readMotifs(system.file(package="Biomotifs", dir="extdata", file="jaspar-insecta.jaspar"), remove.acc=TRUE)
+motifs = readMotifs(system.file(package="PWMEnrich", dir="extdata", file="jaspar-insecta.jaspar"), remove.acc=TRUE)
 
 # check if two things are numerically equal, e.g. to precision of 1e-8
 numEqual = function(x, y, prec=1e-8){

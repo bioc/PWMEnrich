5f9c9f6c8c0ba7015b41b886281096aaa60bb462
Diego Diez <diego10ruiz@gmail.com>
2020-09-05 16:34:29 +0900

    Add PWMEnrich-package.R file to manage dependencies.

---
+++ R/PWMEnrich-package.R
@@ -0,0 +1,16 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @importFrom grDevices col2rgb palette rgb
+#' @importFrom graphics lines par polygon rect text
+#' @importFrom stats cor dlnorm ecdf lm median na.omit optimize pchisq plnorm predict.lm qlnorm quantile
+#' @importFrom utils data
+#' @importFrom S4Vectors metadata
+#' @importFrom seqLogo makePWM plot
+#' @importFrom evd fgev pgev
+#' @importFrom gdata trim
+## usethis namespace: end
+NULL
